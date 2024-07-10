4798c8167ee64caf86fd5d6aa170b37d19185d10
Thato Mongwe <thatomongwe47@gmail.com>
2024-07-10 22:10:59 +0000

    Module with function to pack web_static files into a tgz archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Module with function to pack web_static files into a tgz archive"""
-
-from fabric.api import local
-from datetime import datetime
-from os.path import exists
+"""
+Module with function to pack web_static files into a tgz archive
+"""
+from fabric.api import *
+import time
 
 
 def do_pack():
-    """
-    Creates a tgz archive from the contents of the web_static folder.
-    
-    Returns:
-        The path of the archive if successful, else None.
-    """
+    timestr = time.strftime("%Y%m%d%H%M%S")
     try:
-        now = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_path = f"versions/web_static_{now}.tgz"
-        local("mkdir -p versions")  # Create versions dir if not exists
-        local(f"tar -cvzf {archive_path} web_static")
-        return archive_path
-    except Exception:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(timestr))
+        return ("versions/web_static_{}.tgz".format(timestr))
+    except:
         return None
