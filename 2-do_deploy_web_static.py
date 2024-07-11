3c10ab92e851166f368cdcd1bf9edf568cb1f595
Thato Mongwe <thatomongwe47@gmail.com>
2024-07-11 21:57:49 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,34 @@
 #!/usr/bin/python3
-"""Module with function to deploy a web_static archive to remote servers"""
+from fabric.api import put, run, local, env
+from os import path
 
-from fabric.api import env, put, run
-from os.path import exists
-from datetime import datetime
 
-env.hosts = ['3.84.222.117', '34.207.253.96']  # Replace with your server IPs
+env.hosts = ["3.84.222.117", "34.207.253.96"]
+
 
 def do_deploy(archive_path):
-    """
-    Deploys an archive to remote servers.
-    
-    Args:
-        archive_path (str): Path to the archive file.
-    
-    Returns:
-        True if deployment succeeds, else False.
-    """
+    """Fabric script that distributes
+    an archive to your web server"""
 
-    if not exists(archive_path):
+    if not path.exists(archive_path):
         return False
-
-    file_name = archive_path.split("/")[-1]  
-    no_ext_name = file_name.split(".")[0] 
-    release_path = f"/data/web_static/releases/{no_ext_name}"
-
     try:
-        # Upload and extract the archive to the server.
-        put(archive_path, "/tmp/")
-        run(f"mkdir -p {release_path}")
-        run(f"tar -xzf /tmp/{file_name} -C {release_path}")
-        run(f"rm /tmp/{file_name}") 
-
-        # Move files into the correct place
-        run(f"mv {release_path}/web_static/* {release_path}/")
-        run(f"rm -rf {release_path}/web_static") 
-
-        # Update the symbolic link to point to the new release.
-        run(f"rm -rf /data/web_static/current")
-        run(f"ln -s {release_path} /data/web_static/current")
-
+        tgzfile = archive_path.split("/")[-1]
+        print(tgzfile)
+        filename = tgzfile.split(".")[0]
+        print(filename)
+        pathname = "/data/web_static/releases/" + filename
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(tgzfile, filename))
+        run("rm /tmp/{}".format(tgzfile))
+        run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
         return True
-
     except Exception as e:
-        print(f"Deployment failed: {e}")
         return False
