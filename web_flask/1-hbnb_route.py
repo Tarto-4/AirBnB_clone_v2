d523a2e42237c789b2a1b6347e51f2fa2ed87da0
Thato Mongwe <thatomongwe47@gmail.com>
2024-07-27 15:53:13 +0200

    Flask application demonstrating multiple routes with strict_slashes=False

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,22 @@
-# web_flask/1-hbnb_route.py
-from flask import Flask, render_template
+#!/usr/bin/python3
+"""
+Flask application demonstrating multiple routes with strict_slashes=False
+"""
 
-from models import storage
-
+from flask import Flask
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    storage.close()
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
