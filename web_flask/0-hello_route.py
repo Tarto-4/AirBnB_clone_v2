a9918cc0eb0a50a8858e9df1e698d2232b08cca8
Thato Mongwe <thatomongwe47@gmail.com>
2024-07-27 10:24:38 +0000

    Flask application demonstrating a simple route.

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Flask application demonstrating a simple route.
+
+This script defines a Flask application that listens on 0.0.0.0 port 5000
+and displays "Hello HBNB!" when accessing the root URL (/).
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays "Hello HBNB!" on the root path.
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
