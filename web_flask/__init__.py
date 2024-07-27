70432cbc3bbdb9cbd0360d96f0ddd67e798bde40
Thato Mongwe <thatomongwe47@gmail.com>
2024-07-27 13:28:44 +0000

    initials

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,1 +1,12 @@
-# This file is empty (or can contain initialization code)
+# __init__.py
+from flask import Flask, render_template, request
+from models import storage
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+# Import and register blueprint for each task
