873d49bc6865148e5ae947974ad8dde5e4a45d0f
Thato Mongwe <thatomongwe47@gmail.com>
2024-07-27 15:55:25 +0200

    Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,28 @@
-# web_flask/2-c_route.py
-from flask import Flask, render_template
+#!/usr/bin/python3
+"""
+Flask web application
+"""
 
-from models import storage
-
+from flask import Flask
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    storage.close()
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    return "C {}".format(text.replace('_', ' '))
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
